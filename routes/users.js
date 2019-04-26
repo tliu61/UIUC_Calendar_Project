36174bf9ca3f102fea2a1c494a7ef11cee45d6a2
Matthew Arensdorf <arensdo2@illinois.edu>
2019-04-26 15:39:47 -0500

    users update

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 var express = require('express'),
-    User = require('../models/user'),
-    router = express.Router(),
-    HttpStatus = require('http-status-codes'),
-    bodyParser = require('body-parser'),
-    passwordHash = require('password-hash');
-mongoose = require('mongoose');
+  User = require('../models/user'),
+  router = express.Router(),
+  HttpStatus = require('http-status-codes'),
+  bodyParser = require('body-parser'),
+  passwordHash = require('password-hash');
+  mongoose = require('mongoose');
 
 function checkBody(req) {
   if (typeof req.body.name === 'undefined') {
@@ -28,11 +28,11 @@ function checkBody(req) {
 
 router.get('/', async (req, res) => {
   var where = eval('(' + req.query.where + ')') || {},
-      sort = eval('(' + req.query.sort + ')') || {"dateCreated": 1},
-      select = eval('(' + req.query.select + ')') || {},
-      skip = eval('(' + req.query.skip + ')') || 0,
-      limit = eval('(' + req.query.limit + ')') || 0,
-      count = eval('(' + req.query.count + ')') || false;
+  sort = eval('(' + req.query.sort + ')') || {"dateCreated": 1},
+  select = eval('(' + req.query.select + ')') || {},
+  skip = eval('(' + req.query.skip + ')') || 0,
+  limit = eval('(' + req.query.limit + ')') || 0,
+  count = eval('(' + req.query.count + ')') || false;
 
   try {
     var ret = await User.find(where, select, {skip: skip, limit: limit, sort: sort}).exec();
@@ -62,81 +62,9 @@ router.post('/', async (req, res) => {
       data: ret
     });
   } catch (e) {
-    res.status(404).send(e);
-  }
-});
-
-<<<<<<< HEAD
-router.put("/:id",function(req,res){
-	var query = {_id:req.params.id}
-	var update = req.body;
-	var options = {new:true};
-	try{
-		//console.log("Body:",req.body);
-		if(typeof update.name === 'undefined'){
-			throw "Cannot update a user without name";
-		}
-		if(typeof update.email === 'undefined'){
-			throw "Cannot update a user without email";
-		}
-		User.update(query,update,options,function(err,user){
-			if(err){
-				var json = JSON.stringify({
-					"message": "Cannot update user",
-					"data": user
-				});
-				res.status(HttpStatus.NOT_FOUND).send(json);
-				return;
-			}
-			var json = JSON.stringify({
-				"message": "OK",
-				"data": user
-			});
-			res.status(HttpStatus.OK).send(json);
-		})
-	}
-	catch(e){
-		var json = JSON.stringify({
-			"message": e,
-			"data": ""
-		});
-		console.log("Error");
-		res.status(HttpStatus.BAD_REQUEST).send(json);
-	}
-})
-
-router.delete('/:user',function(req,res){
-	//var options = {new:true};
-	console.log("Remove user:",req.params.user);
-	var query = {_id:req.params.user}
-	result = User.deleteOne(query,function(err){
-		//console.log("Remove user:",user);
-		if(err){
-			var json = JSON.stringify({
-				"message": "Cannot delete user",
-			});
-			res.status(HttpStatus.NOT_FOUND).send(json);
-			return;
-		}
-		var json = JSON.stringify({
-			"message": "OK",
-		});
-		res.status(HttpStatus.OK).send(json);
-	})
-	//console.log("User:=",JSON.stringify(result));
-})
-=======
-router.get('/:email/:password', async (req, res) => {
-  try {
-    res.send({
-      message: 'TEST',
-      email: email,
-      pass: password
-    });
-  } catch (e) {
     res.status(404).send({
       message: 'ERROR',
-      details: e
+      data: e
     });
   }
 });
@@ -149,23 +77,25 @@ router.get('/:id', async (req, res) => {
       data: user
     });
   } catch (e) {
-    res.status(404).send(e);
+    res.status(404).send({
+      message: 'ERROR',
+      data: e
+    });
   }
 });
->>>>>>> d88a29dd0177b98b575f60a1b226394e91cb4db4
 
 router.put('/:id', async (req, res) => {
   try {
-    checkBody(req);
-    var user = await User.findById(req.params.id).exec();
-    user.set(request.body);
-    var ret = await person.save();
+    var ret = await User.findOneAndUpdate({_id: req.params.id}, req.body).exec();
     res.send({
       message: 'OK',
       data: ret
     });
   } catch (e) {
-    res.status(404).send(e);
+    res.status(404).send({
+      message: 'ERROR',
+      data: e
+    });
   }
 });
 
@@ -177,7 +107,10 @@ router.delete('/:id', async (req, res) => {
       data: ret
     });
   } catch (e) {
-    res.status(404).send(e);
+    res.status(404).send({
+      message: 'ERROR',
+      data: e
+    });
   }
 });
 
