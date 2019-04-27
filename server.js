80fb66efe83d8673507461a2e3eec57a8fa8f3fa
Thomas Liu <tliu61@illinois.edu>
2019-04-27 13:31:09 -0500

    hardcoded items added

--- server.js
+++ server.js
@@ -45,6 +45,13 @@ app.use('/api/events',events);
 app.use('/api/users',users);
 require('./routes')(app, router);
 
+// An api endpoint that returns a short list of items
+app.get('/api/users', (req,res) => {
+    var list = ["item1", "item2", "item3"];
+    res.json(list);
+    console.log('Sent list of items');
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'))
 });
